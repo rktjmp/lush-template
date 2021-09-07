7f4c40e98377273dff40233b68964524b2de7d96
Michael Chris Lopez <hello@michaelchris.space>
2021-09-07 17:02:58 +0800

    Add LspCodeLens

--- lua/lush_theme/lush_template.lua
+++ lua/lush_theme/lush_template.lua
@@ -206,6 +206,8 @@ local theme = lush(function()
     -- LspDiagnosticsSignInformation        { }, -- Used for "Information" signs in sign column
     -- LspDiagnosticsSignHint               { }, -- Used for "Hint" signs in sign column
 
+    -- LspCodeLens                          { }, -- Used to color the virtual text of the codelens
+
     -- These groups are for the neovim tree-sitter highlights.
     -- As of writing, tree-sitter support is a WIP, group names may change.
     -- By default, most of these groups link to an appropriate Vim group,

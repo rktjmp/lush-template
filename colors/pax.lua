65785957115603a5ccda0d1421cdcb7652becc75
alunturner <alun.turner@googlemail.com>
2024-03-11 16:24:09 +0000

    fix the broken sed bits and pieces

--- colors/pax.lua
+++ colors/pax.lua
@@ -1,6 +1,6 @@
 -- You probably always want to set this in your vim file
-vim.opt.background = 'dark'
-vim.g.colors_name = 'lush_template'
+vim.opt.background = "dark"
+vim.g.colors_name = "pax"
 
 -- By setting our module to nil, we clear lua's cache,
 -- which means the require ahead will *always* occur.
@@ -14,8 +14,7 @@ vim.g.colors_name = 'lush_template'
 --
 -- The performance impact of this call can be measured in the hundreds of
 -- *nanoseconds* and such could be considered "production safe".
-package.loaded['lush_theme.lush_template'] = nil
+package.loaded["lush_theme.pax"] = nil
 
 -- include our theme file and pass it to lush to apply
-require('lush')(require('lush_theme.lush_template'))
-
+require("lush")(require("lush_theme.pax"))

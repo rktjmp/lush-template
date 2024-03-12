21ad9eee2c7ea721245ff4f5eb826e3808b3dc59
alunturner <alun.turner@googlemail.com>
2024-03-12 15:03:38 +0000

    add some samples

---
+++ samples/javascript.js
@@ -0,0 +1,33 @@
+const thing = require("./some path");
+import thingy from "some/other/path";
+console.log(thingy);
+
+// We are going to have some comments then
+let usSet = "up a bunche of variables";
+const like = true;
+const arr = [];
+const a = { object_with: 123 };
+
+const float = 123.456;
+
+let result = float + a.object_with;
+
+document.window.window.window;
+
+if (arr === []) {
+}
+
+class MyClass {
+  constructor() {
+    return this;
+  }
+
+  mew() {
+    console.log(`mew`);
+  }
+}
+
+let aClassThing = new MyClass();
+aClassThing.mew();
+
+export { result, float };
